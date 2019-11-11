da01beddbbaf3900fce3470120543b0863052a64
Dave Rice <dave@dericed.com>
2019-11-11 15:36:04 -0500

    mm_v2.5.4

--- mm.rb
+++ mm.rb
@@ -1,10 +1,9 @@
 class Mm < Formula
   desc "Media microservices for audiovisual archives"
   homepage "https://github.com/mediamicroservices/mm"
-  url "https://github.com/mediamicroservices/mm/archive/mm_v2.5.3.tar.gz"
-  sha256 "2eb00772fc2591e98c26f9fcf92e03e7f4f2f024975a52c7806a91923a92aeea"
+  url "https://github.com/mediamicroservices/mm/archive/mm_v2.5.4.tar.gz"
+  sha256 "9378c0a8093a258b5f123e46c473ae5bfc18a3f0d56b69414b4d698266c28fd9"
   head "https://github.com/mediamicroservices/mm.git"
-  revision 1
 
   depends_on "cowsay"
   if OS.mac?
